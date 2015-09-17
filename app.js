e2d2fdd02fc65b04b26807342b32eace79f6f7cc
stevenjmarsh <sjm.atl@gmail.com>
2015-09-17 11:30:06 -0400

    adding express

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var mongoose = require('mongoose');
+var express = require('express');
 
 mongoose.connect('mongodb://localhost', function (err) {  // note using default port, so no need to specify
   if (err) throw err;
-
   console.log('connected!');
-  mongoose.disconnect();
+
+  var app = express();
+
+  // set routes
+  app.get('/', function (req, res) {
+    res.status(200).send('hello mongoose blog!');
+  });
+
+  // start listening
+  app.listen(3000, function () {
+    console.log('listening at http://localhost:3000');
+  });
 });
